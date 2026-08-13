3d57f7e8eab7a945a1884b573f8ac76de62384cd
LucaSoftwareEngineer <lucapc40@gmail.com>
2026-08-13 23:21:56 +0200

    feat: send and save entity in azure storage table

--- TableService.cs
+++ TableService.cs
@@ -1,12 +1,31 @@
-﻿namespace AzuriteStorage
+﻿using Azure.Data.Tables;
+
+namespace AzuriteStorage
 {
     public class TableService : IStorageService
     {
-        public Task Execute()
+        public async Task Execute()
         {
             var connectionString = "DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;TableEndpoint=http://127.0.0.1:10002/devstoreaccount1;";
 
-            return Task.CompletedTask;
+            var client = new TableClient(connectionString, "product");
+
+            List<TableEntity> entities = new List<TableEntity>
+            {
+                new("clothes", "1")
+                {
+                    { "Name", "Thirt" }
+                },
+                new("clothes", "2")
+                {
+                    { "Name", "Stivales" }
+                }
+            };
+
+            foreach (var item in entities)
+            {
+                await client.AddEntityAsync(item);
+            }
         }
     }
 }
