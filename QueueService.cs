9710605525c8d19f5c312dda2f483da5b6edf6b2
LucaSoftwareEngineer <lucapc40@gmail.com>
2026-08-13 23:21:28 +0200

    feat: send and save message in azure storage queue

--- QueueService.cs
+++ QueueService.cs
@@ -1,12 +1,15 @@
-﻿namespace AzuriteStorage
+﻿using Azure.Storage.Queues;
+
+namespace AzuriteStorage
 {
     public class QueueService : IStorageService
     {
-        public Task Execute()
+        public async Task Execute()
         {
             var connectionString = "DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;";
 
-            return Task.CompletedTask;
+            var client = new QueueClient(connectionString, "queue1");
+            await client.SendMessageAsync("ciao bro!");
         }
     }
 }
